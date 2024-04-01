a6033fdff19c88d17e9154996566675970e6dc33
Tim Ingham-Dempster <tim_ingham_dempster@hotmail.co.uk>
2024-04-01 17:07:03 +0100

    Add project files.

---
+++ HuygensLight/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace HuygensLight
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+
+            Image = new WriteableBitmap(512, 512, 96, 96, PixelFormats.Bgr32, null);
+
+            CompositionTarget.Rendering += OnRender;
+        }
+
+        private void OnRender(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public WriteableBitmap Image { get; private set; }
+    }
+}
